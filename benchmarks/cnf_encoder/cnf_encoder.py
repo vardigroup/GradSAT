2053179ed4f3b99a41330290d481c1390b8b3ab3
Zhiwei Zhang <45185698+zzwonder@users.noreply.github.com>
2021-08-15 14:00:20 -0500

    Update cnf_encoder.py

--- benchmarks/cnf_encoder/cnf_encoder.py
+++ benchmarks/cnf_encoder/cnf_encoder.py
@@ -9,8 +9,10 @@ def xor2cnf(xor):
         return [[-xor[0],-xor[1],xor[2]], [xor[0],-xor[1],-xor[2]], [-xor[0],xor[1],-xor[2]],[xor[0],xor[1],xor[2]] ]
     if len(xor)==2:
         return [[xor[0],xor[1]],[-xor[0],-xor[1]]]
+    
 def de_xor(llist,start):
     oldstart = start
+    exactFlag = 0
     if len(llist)<=3:
         return [list(llist),],start
     else:
@@ -20,17 +22,21 @@ def de_xor(llist,start):
             if i>=len(llist):
                 break
             elif i==len(llist)-1:
-                oldstart-=1
+                exactFlag = 1
                 break
             else:
                 clist.append([-llist[i],llist[i+1],start+1])
                 start+=1
                 i+=2
-    flist,_ = de_xor(range(oldstart+1,start+1),start)
+    if exactFlag == 1:
+        flist,_ = de_xor([llist[-1]] + list(range(oldstart+1,start+1)),start)
+    else:
+        flist,_ = de_xor(range(oldstart+1,start+1),start)
     return clist+flist,_
 
 def kxor(n):
     return de_xor(range(1,n+1),n+1)
+
 def xor100():
     f = open('benchmarks/sxor/x10.cnf','w+')
     n=10
